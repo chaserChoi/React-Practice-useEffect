de41775cf144976f634b31e58dcbc28c17cd7b7c
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-01-11 02:54:11 +0900

    198. useEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -13,19 +13,34 @@ function App() {
   const [availablePlaces, setAvailablePlaces] = useState([]); // [
   const [pickedPlaces, setPickedPlaces] = useState([]);
 
+  // 198. useEffect Hook
+  // useEffect(1.() => {}, 2.[]) : 1. 부수 효과를 묶어줄 함수, 2. 의존성 배열
+  // 앱 컴포넌트 함수 실행 완료 후, 부수 효과 함수 실행 -> 의존성 X -> 재실행 X
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.altitude,
+        position.coords.longitude
+      );
+
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
+
   // 196~197 : Side Effect(부수 효과)
   // 현재 컴포넌트 렌더링 과정에 직접적으로 관여하지 않는 작업
   // But, 상태 업데이트 함수 호출 -> 상태가 포함된 컴포넌트 함수 재실행 -> 무한 루프 발생
   // -> useEffect Hook 사용!
-  navigator.geolocation.getCurrentPosition((position) => {
-    const sortedPlaces = sortPlacesByDistance(
-      AVAILABLE_PLACES,
-      position.coords.altitude,
-      position.coords.longitude
-    );
+  // navigator.geolocation.getCurrentPosition((position) => {
+  //   const sortedPlaces = sortPlacesByDistance(
+  //     AVAILABLE_PLACES,
+  //     position.coords.altitude,
+  //     position.coords.longitude
+  //   );
 
-    setAvailablePlaces(sortedPlaces)
-  });
+  //   setAvailablePlaces(sortedPlaces)
+  // });
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -79,7 +94,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting places by distance ..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
